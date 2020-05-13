ffe84621aadb4e24b3c97bd23ca79804010fdcd0
michael_gvdw <michael@fragrant-chios.com>
2020-05-13 18:16:50 +0200

    added the links in the admin page

--- app/resources/views/layout/adminnav.php
+++ app/resources/views/layout/adminnav.php
@@ -2,4 +2,7 @@
 <a class="adminnavbutton" href='adminpage.php' title='Messages'>Admin page</a>
 <a class="adminnavbutton" href='messages.php'>Messages</a>
 <a class="adminnavbutton" href='articlemanager.php'>Article Manager</a>
+<a class="adminnavbutton" href="listOfCourses.php">Courses</a>
+<a class="adminnavbutton" href="listOfCoaches.php">Coaches</a>
+<a class="adminnavbutton" href="listOfRegattas.php">Regattas</a>
 </div>
